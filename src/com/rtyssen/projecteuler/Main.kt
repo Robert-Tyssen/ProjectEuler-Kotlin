893beae569c5af4baa2e5ed4a9767a93e03b141e
Robert Tyssen <rob.tyssen@gmail.com>
2021-07-05 17:16:50 -0400

    Added number to function mapping

--- src/com/rtyssen/projecteuler/Main.kt
+++ src/com/rtyssen/projecteuler/Main.kt
@@ -1,15 +1,40 @@
 package com.rtyssen.projecteuler
 
-fun main(args: Array<String>){
+import com.rtyssen.projecteuler.problems.*
+import java.lang.Exception
+import kotlin.system.measureTimeMillis
 
-    val problemNumber: Int
-    val timeInMillies: Long
-    val result: String
+fun main(){
+
+    val problemNumber = 7
+    var res = ""
 
     println("\n********** PROJECT EULER **********\n")
-    println("Executing problem ...")
-    println("Result = ...")
-    println("Execution time = ... ms")
+    println("Executing problem # ${problemNumber}")
+
+    //val f = getFunctionForProblem(problemNumber)
+    val f = ::"problem1".name
+    val millis = measureTimeMillis { res = f().toString() }
+
+    println("Result = $res")
+    println("Execution time = $millis ms")
     println("\n********** COMPLETE **********\n")
+
+}
+
+// Get the function for the inputted value n
+// TODO - would reflection be better for this?
+private fun getFunctionForProblem(n: Int): () -> Any = when (n) {
+    // Map input number to executable function
+    1  -> ::problem1
+    2  -> ::problem2
+    6  -> ::problem6
+    7  -> ::problem7
+    50 -> ::problem50
+
+    // No executable function found, return generic message
+    else -> {
+        fun() = "PROBLEM NOT IMPLEMENTED"
+    }
 
 }
